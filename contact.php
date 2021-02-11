9e0ac9e125ecb2a53251cc06a3926c9fd2da48e4
taikoro-san <246810ab90@gmail.com>
2021-02-11 19:20:38 +0900

    releaseブランチ作成

--- contact.php
+++ contact.php
@@ -1,25 +1,13 @@
-<?php
-echo 'Linebot早く完成させなきゃ。';
+git init = gitリポジトリとして初期化
+git add ファイル名 = *選択したファイルをステージング
+git commit -m "コメント" = *ローカルリポジトリにコミット
+git push origin master = *origin? master?ブランチにプッシュ(要調査)
+origin = リモートリポジトリのアクセス先(絶対パス)に対してGitがデフォでつける名前
 
-// git init = gitリポジトリとして初期化
-// git add ファイル名 = *選択したファイルをステージング
-// git commit -m "コメント" = *ローカルリポジトリにコミット
-// git push origin master = *origin? master?ブランチにプッシュ(要調査)
-// origin = リモートリポジトリのアクセス先(絶対パス)に対してGitがデフォでつける名前
-
-// githubのユーザー名とパスワードの入力を求められる
-// git status = ステージングの状況を確認する
-// git log = プッシュした履歴を見る
-// *初期状態(githubと連携していない場合)でコマンドを打つとerror発生。
-// git config --globalコマンド実行後ユーザー名とメール設定
-// git remote add origin URLを入力 でプッシュしたいリポジトリを設定する。
-// コミット忘れてpushしたらEverything up-to-dateと返ってくる
-?>
-<!DOCTYPE htm>
-<head lang="ja">
-<meta charset="UTF-8">
-<title>課題20git</title>
-</head>
-<body>
-</body>
-</html>
+githubのユーザー名とパスワードの入力を求められる
+git status = ステージングの状況を確認する
+git log = プッシュした履歴を見る
+*初期状態(githubと連携していない場合)でコマンドを打つとerror発生。
+git config --globalコマンド実行後ユーザー名とメール設定
+git remote add origin URLを入力 でプッシュしたいリポジトリを設定する。
+コミット忘れてpushしたらEverything up-to-dateと返ってくる
